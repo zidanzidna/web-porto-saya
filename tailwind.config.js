399b8ed13d06f3811bf01dca41c9ebe58883c7f1
zidanzidna <zidanzidnarifan@gmail.com>
2026-01-16 22:46:51 +0700

    ganti ke  tailwind v3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/**/*.html",
+    "./src/**/*.html",
+    "./*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
