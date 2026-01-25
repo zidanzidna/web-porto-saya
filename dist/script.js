4c915f662b8ba59457f05b76778ea8fa8e9afd34
zidanzidna <zidanzidnarifan@gmail.com>
2026-01-25 17:33:35 +0700

    add fonts

--- dist/script.js
+++ dist/script.js
@@ -1,17 +1,19 @@
 // Di component
 import localFont from './font/varsity_regular.ttf'
+import localFont from './font/PressStart2P-Regular.ttf'
+import localFont from './font/VT323-Regular.ttf'
 
 const varsity = localFont({
-  src: './fonts/varsity_regular.ttf',
+  src: './font/varsity_regular.ttf',
   variable: '--font-varsity',
 })
 
 const start = localFont({
-  src: './fonts/PressStart2P-Regular.ttf',
+  src: './font/PressStart2P-Regular.ttf',
   variable: '--font-heading',
 })
 
 const vt323 = localFont({
-  src: './fonts/VT323-Regular.ttf',
+  src: './font/VT323-Regular.ttf',
   variable: '--font-body',
 })
