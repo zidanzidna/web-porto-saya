770a43266902d08da6fa0daa9723723487554fb7
zidanzidna <zidanzidnarifan@gmail.com>
2026-01-25 17:31:18 +0700

    add fonts

--- dist/script.js
+++ dist/script.js
@@ -5,3 +5,13 @@ const varsity = localFont({
   src: './fonts/varsity_regular.ttf',
   variable: '--font-varsity',
 })
+
+const start = localFont({
+  src: './fonts/PressStart2P-Regular.ttf',
+  variable: '--font-heading',
+})
+
+const vt323 = localFont({
+  src: './fonts/VT323-Regular.ttf',
+  variable: '--font-body',
+})
